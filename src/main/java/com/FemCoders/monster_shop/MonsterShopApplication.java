7a78c0169eeda850649fa49e1a34c86065b39021
Nia <niaespinal@outlook.com>
2025-06-16 21:19:21 +0200

    First commit for monster-shop

---
+++ src/main/java/com/FemCoders/monster_shop/MonsterShopApplication.java
@@ -0,0 +1,13 @@
+package com.FemCoders.monster_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FemCoders/monster_shop/MonsterShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FemCoders.monster_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
