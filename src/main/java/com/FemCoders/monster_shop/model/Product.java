36630bb5772c759f2cad40f98ebbee416868bf82
Nia <niaespinal@outlook.com>
2025-06-17 12:26:54 +0200

    Add validations to models

--- src/main/java/com/FemCoders/monster_shop/model/Product.java
+++ src/main/java/com/FemCoders/monster_shop/model/Product.java
@@ -4,18 +4,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Min(value = 0, message = "Price must be positive")
     private double price;
+
+    @NotBlank(message = "Image URL is required")
     private String imageUrl;
+
+    @Min(0)
+    @Max(5)
     private double rating;
+
+    @Min(0)
     private int reviewCount;
+
     private boolean featured;
 
 }
--- src/main/java/com/FemCoders/monster_shop/model/Review.java
+++ src/main/java/com/FemCoders/monster_shop/model/Review.java
@@ -1,15 +1,25 @@
 package com.FemCoders.monster_shop.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Review {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @Min(0)
+    @Max(5)
     private double rating;
+
+    @NotBlank(message = "Review body cannot be empty")
     private String body;
 
     @ManyToOne
