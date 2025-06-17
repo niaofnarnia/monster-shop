503b474ca1ef1d85be63e77a722ea69699188465
Nia <niaespinal@outlook.com>
2025-06-17 12:08:29 +0200

    Add Product and Review services

---
+++ src/main/java/com/FemCoders/monster_shop/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.FemCoders.monster_shop.service;
+
+import com.FemCoders.monster_shop.repository.ProductRepository;
+import com.FemCoders.monster_shop.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
