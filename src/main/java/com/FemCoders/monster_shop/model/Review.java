5e241d2c8ebbc86fe88344cb2c9dea46065e4343
Nia <niaespinal@outlook.com>
2025-06-18 01:47:29 +0200

    Add getters and setters to model

--- src/main/java/com/FemCoders/monster_shop/model/Review.java
+++ src/main/java/com/FemCoders/monster_shop/model/Review.java
@@ -12,19 +12,55 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @NotBlank(message = "Username is required")
     private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     @Min(0)
     @Max(5)
     private double rating;
 
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
     @NotBlank(message = "Review body cannot be empty")
     private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
+
+    public Product getProduct() {
+        return product;
+    }
 
     public void setProduct(Product product) {
         this.product = product;
